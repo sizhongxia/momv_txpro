afe7e4ff5f04b82eb2262dbc986bdffd8e7e1c93
sizhongxia <sizhongxia@haijupai.com>
2017-11-22 17:55:20 +0800

    移除测试

--- tm-pro-es/src/test/java/org/tm/pro/es/TestElasticUtil.java
+++ tm-pro-es/src/test/java/org/tm/pro/es/TestElasticUtil.java
@@ -6,12 +6,12 @@ public class TestElasticUtil {
 
 	@Test
 	public void getTest() {
-		ElasticUtil elasticUtil = new ElasticUtil();
-		elasticUtil.setBaseUrl("http://127.0.0.1:9200/");
-		System.out.println(elasticUtil.getHttpStatusInfo("ppc_log_test-20170930", "access"));
-		System.out.println(elasticUtil.getBackFlowInfo("ppc_log_test-20170930", "access"));
-		System.out.println(elasticUtil.getCacheStatusInfo("ppc_log_test-20170930", "access"));
-		System.out.println(elasticUtil.getUrlInfo("ppc_log_test-20170930", "access"));
+//		ElasticUtil elasticUtil = new ElasticUtil();
+//		elasticUtil.setBaseUrl("http://127.0.0.1:9200/");
+//		System.out.println(elasticUtil.getHttpStatusInfo("ppc_log_test-20170930", "access"));
+//		System.out.println(elasticUtil.getBackFlowInfo("ppc_log_test-20170930", "access"));
+//		System.out.println(elasticUtil.getCacheStatusInfo("ppc_log_test-20170930", "access"));
+//		System.out.println(elasticUtil.getUrlInfo("ppc_log_test-20170930", "access"));
 	}
 
 }
