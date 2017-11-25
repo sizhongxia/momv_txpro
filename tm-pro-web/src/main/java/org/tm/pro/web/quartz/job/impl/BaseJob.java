04ad2f9b56a48efce74a49fa440a98328525ef28
sizhongxia <sizhongxia@haijupai.com>
2017-11-25 11:24:26 +0800

    改造BaseJob

--- tm-pro-web/src/main/java/org/tm/pro/web/quartz/job/impl/BaseJob.java
+++ tm-pro-web/src/main/java/org/tm/pro/web/quartz/job/impl/BaseJob.java
@@ -10,24 +10,40 @@ import org.tm.pro.web.cache.SystemInfoCacheUtil;
 import org.tm.pro.web.quartz.job.RunJob;
 import org.tm.pro.web.quartz.model.Job;
 
+import com.tm.pro.redis.util.RedisUtil;
+
 @Component
 public class BaseJob implements RunJob {
 
 	@Autowired
 	UserService userService;
+	@Autowired
+	RedisUtil redisUtil;
 
 	@Override
 	public void run() {
+		String info = null, clazz = null, key = null;
 		List<Job> jobs = SystemInfoCacheUtil.jobs;
 		if (jobs != null && !jobs.isEmpty()) {
 			for (Job job : jobs) {
 				if (job.getJobClassName().equals(this.getClass().getName())) {
+					clazz = job.getJobClassName();
 					job.setUpdateTime(TmDateUtil.format(System.currentTimeMillis(), "yyyy-MM-dd HH:mm:ss"));
 					break;
 				}
 			}
 		}
-
+		key = "system:job:runlog:" + clazz;
+		info = redisUtil.get(key);
+		if (info == null) {
+			info = "执行时间：" + System.currentTimeMillis();
+		} else {
+			info += "<br/>执行时间：" + System.currentTimeMillis();
+		}
+		if (info.length() > 1024) {
+			info = info.substring(info.length() - 1025, info.length() - 1);
+		}
+		redisUtil.set(key, info);
 		try {
 			Thread.sleep(3067);
 		} catch (InterruptedException e) {
