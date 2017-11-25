314268dc15811b942004ea05ce963c11c863aee7
sizhongxia <sizhongxia@haijupai.com>
2017-11-25 21:02:08 +0800

    基本图表开发

--- tm-pro-web/src/main/java/org/tm/pro/web/controller/manage/DataController.java
+++ tm-pro-web/src/main/java/org/tm/pro/web/controller/manage/DataController.java
@@ -4,7 +4,9 @@ import java.text.DateFormat;
 import java.text.SimpleDateFormat;
 import java.util.ArrayList;
 import java.util.Date;
+import java.util.HashMap;
 import java.util.List;
+import java.util.Map;
 
 import javax.servlet.http.HttpServletRequest;
 
@@ -39,6 +41,35 @@ public class DataController extends BaseController {
 			_n = now - (num - i) * 30000;
 			item.setTime(df.format(new Date(_n)));
 			item.setValue(Math.random() * 1000);
+			list.add(item);
+		}
+
+		arm.setCode(200);
+		arm.setStatus(true);
+		arm.setList(list);
+		return arm;
+	}
+
+	@ResponseBody
+	@RequiresAuthentication
+	@RequestMapping(value = "/line")
+	public ApiResultMap line(HttpServletRequest request) {
+		ApiResultMap arm = new ApiResultMap();
+
+		int num = 100;
+		long now = System.currentTimeMillis();
+		List<Map<String, Object>> list = new ArrayList<>();
+		Map<String, Object> item = null;
+		long _n = 0;
+
+		DateFormat df = new SimpleDateFormat("yyyy-MM-dd HH:mm:ss");
+
+		for (int i = 0; i < num; i++) {
+			item = new HashMap<>();
+			_n = now - (num - i) * 1000;
+			item.put("date", df.format(new Date(_n)));
+			item.put("吐出流量", Math.random() * 1000);
+			item.put("回源流量", Math.random() * 1000);
 			list.add(item);
 		}
 
